abf4ceae90cd4f84b037099fa0a994b9fb2e9570
DanilFilikov <d.filikov@tg.dunice.net>
2023-04-13 12:30:44 +0300

    even more code improvements

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,32 +6,36 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+
         while (true) {
-            System.out.print("\n Выберите задание: ");
-            int smth = in.nextInt();
-            switch (smth) {
+
+            Scanner in = new Scanner(System.in);
+            System.out.print("\n Выберите задание:\n 1)Вывести типы данных\n 2)Сравнение чисел\n" +
+                    " 3)Вывод всех простых чисел в заданном диапазоне\n 4)Корзина\n 5)Проверка пароля\n ");
+            switch (in.nextInt()) {
                 case 1:
-                    task1();
+                    getTask1();
                     break;
                 case 2:
-                    task2();
+                    getTask2(in);
                     break;
                 case 3:
-                    task3();
+                    getTask3(in);
                     break;
                 case 4:
-                    task4();
+                    getTask4(in);
                     break;
                 case 5:
-                    task5();
+                    getTask5(in);
                     break;
             }
         }
     }
 
-    public static void task1() {
+    public static void getTask1() {
+
         String string = "Строка";
         char ch = 'а';
         int[] array = {1, 2, 3, 4};
@@ -48,42 +52,55 @@ public class Main {
         System.out.println("bool = " + bool);
     }
 
-    public static void task2() {
-        Scanner in = new Scanner(System.in);
+    public static void getTask2(Scanner in) {
+
         System.out.println("Введите 2 числа:");
         int x = in.nextInt();
         int y = in.nextInt();
-        if (x > y) {
-            System.out.println(x + " больше");
+
+        if (x >= y) {
+            if (x > y) {
+                System.out.println(x + " больше");
+            } else {
+                System.out.println("Числа равны");
+            }
         } else {
             System.out.println(y + " больше");
         }
     }
 
-    public static void task3() {
-        Scanner in = new Scanner(System.in);
+    public static void getTask3(Scanner in) {
+
         System.out.println("Введите минимальное и максимальное число:");
         int min = in.nextInt();
         int max = in.nextInt();
-        System.out.print(min + " ");
-        while (min != max) {
-            min += 1;
-            System.out.print(min + " ");
+        boolean prime;
+        for (int i = min; i <= max - 1; i++) {
+            prime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    prime = false;
+                }
+            }
+            if (prime & (i > 2)) {
+                System.out.print(i + " ");
+            }
         }
     }
 
-    public static void task4() {
-        Scanner in = new Scanner(System.in);
+    public static void getTask4(Scanner in) {
+
         List<String> list = new ArrayList<>();
-        System.out.println("Наполинте корзину");
-        while (list.size() != 10) {
+        System.out.println("Введите кол-во желаемых товаров, затем вводите товар");
+        int product = in.nextInt();
+        while (list.size() != product) {
             list.add(in.next());
             System.out.println(list);
         }
     }
 
-    public static void task5() {
-        Scanner in = new Scanner(System.in);
+    public static void getTask5(Scanner in) {
+
         System.out.println("Задайте пароль:");
         String password = in.next();
         while (!(password.equals(in.next()))) {
